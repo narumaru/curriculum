041d256ecc3fbaf6aa52d65c03fd7fa665999333
narumaru <39397179+narumaru@users.noreply.github.com>
2022-05-15 14:55:59 +0900

    FizzBuzz問題
    FizzBuzz問題(3=Fizz!,5=Buzz!,3&5=FizzBuzz!!)

---
+++ PHPjob/3-1/index.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+ 
+for ($i = 1; $i <= 100; $i++) {
+
+    if (($i % 3 === 0) && ($i % 5 === 0)) {
+        echo 'FizzBuzz!!';
+
+    } elseif ($i % 3 === 0) {
+        echo 'Fizz!';
+
+    } elseif ($i % 5 === 0) {
+        echo 'Buzz!';
+
+    } else {
+        echo $i;
+    }
+    
+    echo '<br>';
+}
+?>
